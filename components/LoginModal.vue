e706cf4df16a73ce578729cd65b79c65f3593fd4
Kostya <ckutls228@gmail.com>
2024-09-16 21:13:22 +0300

    added styled for login-modal with adaptive

--- components/LoginModal.vue
+++ components/LoginModal.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="modal">
+  <div class="modal font-default">
     <div class="modal__content">
       <div class="modal__line">
         <svg @click="closeModal" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px">    <path d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z"/></svg>
+      </div>
+      <div class="input-tag__content">
+        <h2>#</h2>
+        <input type="text" placeholder="YOUR TAG" maxlength="15" v-model="tagUser">
+      </div>
+      <div class="input-button__content">
+        <button>
+          Submit
+        </button>
       </div>
     </div>
   </div>
@@ -11,6 +20,8 @@
 <script setup lang="ts">
   const emit = defineEmits(['closeLoginModal'])
   const elements = ['header', 'main', 'footer'].map(tag => document.querySelector(tag))
+
+  const tagUser = ref('')
 
   function setStyled(opacity: string = 'unset', pointerEvents: string = 'unset'): void {
     elements.forEach(elem => {
@@ -30,6 +41,10 @@
 </script>
 
 <style scoped>
+input {
+  all: unset;
+}
+
 .modal {
   position: absolute;
   top: 20%;
@@ -39,7 +54,7 @@
 }
 
 .modal__content {
-  background: var(--darkest-main-color);
+  background: var(--lightest-main-color);
   width: 400px;
   height: 200px;
   border-radius: 15px;
@@ -56,5 +71,53 @@
 }
 .modal__line svg:hover {
   padding: 2px;
+}
+
+.input-tag__content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 40px;
+}
+.input-club__content input, .input-tag__content input {
+  width: 160px;
+  height: 20px;
+  border: 2px solid var(--darkest-main-color);
+  font-size: 16px;
+  text-transform: uppercase;
+}
+.input-club__content h2, .input-tag__content h2 {
+  background: var(--darkest-main-color);
+  height: 24px;
+  width: 25px;
+  text-align: center;
+  padding-top: 5px;
+}
+
+.input-button__content {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+.input-button__content button {
+  width: 120px;
+  height: 30px;
+  cursor: pointer;
+  background: var(--main-color);
+  border-radius: 10px;
+  border: none;
+  color: #fff;
+  font-size: 1.5rem;
+  font-family: 'Lilita One', sans-serif;
+  transition: .2s ease-in;
+}
+.input-button__content button:hover {
+  background: #003994;
+}
+
+@media not all and (min-width: 640px) {
+  .modal__content {
+    width: 300px;
+  }
 }
 </style>
